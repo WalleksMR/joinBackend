f40ba5502fdbc6dbe7c8310a3f6102eb3eeba7ea
WalleksMR <walleksmiranda@gmail.com>
2022-02-21 16:39:04 -0300

    chore: add file of config connection typeorm

---
+++ backend/ormconfig.ts
@@ -0,0 +1,14 @@
+export default {
+  type: process.env.DB_CONNECTION,
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
+  synchronize: false,
+  migrations: ['./src/shared/**/migrations/*.ts'],
+  entities: ['src/modules/**/entities/*.ts'],
+  cli: {
+    migrationsDir: './src/shared/infra/typeorm/migrations',
+  },
+};
