8b1232560a3e066510c6bc936dc225f062f2f264
WalleksMR <walleksmiranda@gmail.com>
2022-02-22 19:15:25 -0300

    refactor: middleware to exception error and database config

--- backend/src/shared/infra/http/server.ts
+++ backend/src/shared/infra/http/server.ts
@@ -1,13 +1,34 @@
 import 'reflect-metadata';
-import express from 'express';
+import 'dotenv/config';
+import express, { NextFunction, Request, Response } from 'express';
+import 'express-async-errors';
+import '../../container';
+
+import { AppError } from '@shared/errors/AppError';
+import createConnection from '@shared/infra/typeorm';
 
 import { routes } from './routers';
 
+createConnection('database_mysql');
 const port = 3232;
 
 const app = express();
 app.use(express.json());
 app.use(routes);
+
+app.use(
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  (err: Error, _request: Request, response: Response, _next: NextFunction) => {
+    if (err instanceof AppError) {
+      return response.status(err.statusCode).json({ message: err.message });
+    }
+
+    return response.status(500).json({
+      status: 'error',
+      message: `Internal server error - ${err.message}`,
+    });
+  },
+);
 
 app.listen(port, () => {
   console.log(`🚀 server run in port ${port}`);
