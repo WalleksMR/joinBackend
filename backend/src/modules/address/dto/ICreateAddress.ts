eb666a79e427ce012e0e4dd02094715bfb7d4a9e
WalleksMR <walleksmiranda@gmail.com>
2022-02-22 18:55:31 -0300

    chore: add create dto to address

---
+++ backend/src/modules/address/dto/ICreateAddress.ts
@@ -0,0 +1,13 @@
+export interface ICreateAddress {
+  id?: string;
+  client_id: string;
+  street: string;
+  number: string;
+  complement: string;
+  neighborhood: string;
+  city: string;
+  state: string;
+  zip_code: string;
+  latitude: string;
+  longitude: string;
+}
