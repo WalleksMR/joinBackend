e81709850e9bcfa30113e356096d3c77a8e23511
WalleksMR <walleksmiranda@gmail.com>
2022-02-22 19:17:39 -0300

    chore: add a new router to list all clients

--- backend/src/shared/infra/http/routers/clients.routes.ts
+++ backend/src/shared/infra/http/routers/clients.routes.ts
@@ -1,7 +1,11 @@
+import { CreateClientController } from '@modules/clients/useCases/createClient/CreateClientController';
+import { ListClientController } from '@modules/clients/useCases/listClient/ListClientController';
 import { Router } from 'express';
 
 export const routerClient = Router();
 
-routerClient.get('/', (req, res) => {
-  return res.send();
-});
+const createClientController = new CreateClientController();
+const listClientController = new ListClientController();
+
+routerClient.post('/', createClientController.handle);
+routerClient.get('/', listClientController.handle);
