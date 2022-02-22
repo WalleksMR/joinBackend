bab9b2905917f77a6d504213a46aa2697fb82ede
WalleksMR <walleksmiranda@gmail.com>
2022-02-22 19:12:22 -0300

    chore: add two method

--- backend/src/modules/clients/repositories/IClientsRepository.ts
+++ backend/src/modules/clients/repositories/IClientsRepository.ts
@@ -3,6 +3,8 @@ import { Client } from '../infra/typeorm/entities/Client';
 
 export interface IClientsRepository {
   create(data: ICreateClient): Promise<Client>;
+  findById(id: string): Promise<Client>;
+  findByCnpj(cnpj: string): Promise<Client>;
   listAll(): Promise<Client[]>;
   delete(id: string): Promise<void>;
 }
