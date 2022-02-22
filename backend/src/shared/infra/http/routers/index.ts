3d1b54490ec03daa3b6e4b3ad9d10c68061135da
WalleksMR <walleksmiranda@gmail.com>
2022-02-22 19:18:13 -0300

    chore: import router address

--- backend/src/shared/infra/http/routers/index.ts
+++ backend/src/shared/infra/http/routers/index.ts
@@ -1,7 +1,9 @@
 import { Router } from 'express';
 
+import { addressRoutes } from './address.routes';
 import { routerClient } from './clients.routes';
 
 export const routes = Router();
 
 routes.use('/clients', routerClient);
+routes.use('/address', addressRoutes);
