3ee7c7f89fee86f6ebfd8d09f05512a13642721a
WalleksMR <walleksmiranda@gmail.com>
2022-02-23 10:34:28 -0300

    refactor: server isolated

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -1,34 +1,6 @@
-import 'reflect-metadata';
-import 'dotenv/config';
-import express, { NextFunction, Request, Response } from 'express';
-import 'express-async-errors';
-import '../../container';
+import { app } from './app';
 
-import { AppError } from '@shared/errors/AppError';
-import createConnection from '@shared/infra/typeorm';
-
-import { routes } from './routers';
-
-createConnection('database_mysql');
 const port = 3232;
-
-const app = express();
-app.use(express.json());
-app.use(routes);
-
-app.use(
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  (err: Error, _request: Request, response: Response, _next: NextFunction) => {
-    if (err instanceof AppError) {
-      return response.status(err.statusCode).json({ message: err.message });
-    }
-
-    return response.status(500).json({
-      status: 'error',
-      message: `Internal server error - ${err.message}`,
-    });
-  },
-);
 
 app.listen(port, () => {
   console.log(`🚀 server run in port ${port}`);
