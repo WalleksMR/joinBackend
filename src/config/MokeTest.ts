649da612b8fe59219d734911ac1aeb418a10ed6d
WalleksMR <walleksmiranda@gmail.com>
2022-02-23 12:35:01 -0300

    chore: add new moke to address

--- src/config/MokeTest.ts
+++ src/config/MokeTest.ts
@@ -5,7 +5,7 @@ export default {
     corporate_name: 'Razao social',
     contact: '5594991008899',
   },
-  UnderfinedClient: {
+  UndefinedClient: {
     name: '',
     cnpj: '',
     corporate_name: '',
@@ -16,5 +16,28 @@ export default {
     cnpj: '80212855000115',
     corporate_name: 'Razao social update',
     contact: '5594991008899',
+  },
+  CreateAddress: {
+    street: 'Rua Rex',
+    number: '1234',
+    complement: 'Casa',
+    neighborhood: 'Bairro Local',
+    city: 'Ourilândia',
+    state: 'PA',
+    zip_code: '68390000',
+    latitude: '22222',
+    longitude: '333333',
+  },
+
+  UndefinedAddress: {
+    street: '',
+    number: '',
+    complement: '',
+    neighborhood: '',
+    city: '',
+    state: '',
+    zip_code: '',
+    latitude: '',
+    longitude: '',
   },
 };
