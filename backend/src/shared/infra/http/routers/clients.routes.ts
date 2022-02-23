a67be69b6524e5a8c896f0e5a7014f3f5309668a
WalleksMR <walleksmiranda@gmail.com>
2022-02-23 08:44:05 -0300

    chore: add new router to update client

--- backend/src/shared/infra/http/routers/clients.routes.ts
+++ backend/src/shared/infra/http/routers/clients.routes.ts
@@ -1,11 +1,14 @@
 import { CreateClientController } from '@modules/clients/useCases/createClient/CreateClientController';
 import { ListClientController } from '@modules/clients/useCases/listClient/ListClientController';
+import { UpdateClientController } from '@modules/clients/useCases/updateClient/UpdateClientController';
 import { Router } from 'express';
 
 export const routerClient = Router();
 
 const createClientController = new CreateClientController();
 const listClientController = new ListClientController();
+const updateClientController = new UpdateClientController();
 
 routerClient.post('/', createClientController.handle);
 routerClient.get('/', listClientController.handle);
+routerClient.patch('/:id', updateClientController.handle);
