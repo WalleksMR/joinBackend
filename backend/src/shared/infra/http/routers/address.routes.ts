f687890f3915fc377b7468f6427e3e028f00db56
WalleksMR <walleksmiranda@gmail.com>
2022-02-23 08:43:46 -0300

    chore: add new router to update address

--- backend/src/shared/infra/http/routers/address.routes.ts
+++ backend/src/shared/infra/http/routers/address.routes.ts
@@ -1,8 +1,11 @@
 import { CreateAddressController } from '@modules/address/useCases/createAddress/CreateAddressController';
+import { UpdateAddressController } from '@modules/address/useCases/updateAddress/UpdateAddressController';
 import { Router } from 'express';
 
 export const addressRoutes = Router();
 
 const createAddressController = new CreateAddressController();
+const updateAddressController = new UpdateAddressController();
 
 addressRoutes.post('/:client_id', createAddressController.handle);
+addressRoutes.patch('/update/:id', updateAddressController.handle);
