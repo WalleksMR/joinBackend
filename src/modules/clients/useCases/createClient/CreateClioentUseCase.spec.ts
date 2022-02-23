9bf4fdeba81d7d5c432ac9244f5374ca6199ed0b
WalleksMR <walleksmiranda@gmail.com>
2022-02-23 09:25:28 -0300

    test: should not be able create a new client if cnpj already exists

--- src/modules/clients/useCases/createClient/CreateClioentUseCase.spec.ts
+++ src/modules/clients/useCases/createClient/CreateClioentUseCase.spec.ts
@@ -1,6 +1,8 @@
 import MokeTest from '@config/MokeTest';
 import { ClientsRepositoryInMemory } from '@modules/clients/repositories/in-memory/ClientsRepositoryInMemory';
 
+import { AppError } from '@shared/errors/AppError';
+
 import { CreateClientUseCase } from './CreateClientUseCase';
 
 describe('Use Case Client', () => {
@@ -17,5 +19,12 @@ describe('Use Case Client', () => {
 
     expect(client).toHaveProperty('id');
     expect(client.cnpj).toBe(MokeTest.CreateClient.cnpj);
+  });
+
+  it('should not be able create a new client if cnpj already exists', () => {
+    expect(async () => {
+      await createClientUseCase.execute(MokeTest.CreateClient);
+      await createClientUseCase.execute(MokeTest.CreateClient);
+    }).rejects.toBeInstanceOf(AppError);
   });
 });
